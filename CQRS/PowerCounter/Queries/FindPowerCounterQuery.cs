87d5a08e334d7026582dedab0152de0fb65f7eea
Mehran Dhn <FloydianMehran@live.com>
2020-09-12 20:42:02 +0430

    PowerCounter CQRS Queries And Handlers

---
+++ CQRS/PowerCounter/Queries/FindPowerCounterQuery.cs
@@ -0,0 +1,18 @@
+﻿using EnergyAPIClient.Model.DTO;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnergyAPIClient.CQRS.PowerCounter.Queries
+{
+    public class FindPowerCounterQuery : IRequest<PowerSrcInfoDto>
+    {
+        public FindPowerCounterQuery(string tagid)
+        {
+            TagIdentity = tagid;
+        }
+        public string TagIdentity { get; set; }
+    }
+}
---
+++ CQRS/PowerCounter/Queries/PowerCounterHandler.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using EnergyAPIClient.Model.DTO;
+using EnergyAPIClient.ORM;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EnergyAPIClient.CQRS.PowerCounter.Queries
+{
+    public class PowerCounterHandler
+            :
+        IRequestHandler<GetPowerCounterListQuery, IEnumerable<PowerSrcInfoDto>>,
+        IRequestHandler<GetPowerCounterWhereQuery, IEnumerable<PowerSrcInfoDto>>,
+        IRequestHandler<FindPowerCounterQuery, PowerSrcInfoDto>
+    {
+        private readonly EnergyDbContext _context;
+        private readonly IMapper _mapper;
+        public PowerCounterHandler(EnergyDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<PowerSrcInfoDto>> Handle(GetPowerCounterListQuery request, CancellationToken cancellationToken)
+        {
+            var powerCounterEntities = await _context.PowerSrcInfo.Include(c => c.TargetCounter).ToListAsync();
+            List<PowerSrcInfoDto> dtoList = _mapper.Map<List<PowerSrcInfoDto>>(powerCounterEntities);
+            return dtoList;
+        }
+        public async Task<IEnumerable<PowerSrcInfoDto>> Handle(GetPowerCounterWhereQuery request, CancellationToken cancellationToken)
+        {
+            var powerCounterEntities = await _context.PowerSrcInfo
+                .Where(request.Condition)
+                .AsQueryable()
+                .Include(c => c.TargetCounter)
+                .ToListAsync();
+            List<PowerSrcInfoDto> dtoList = _mapper.Map<List<PowerSrcInfoDto>>(powerCounterEntities);
+            return dtoList;
+        }
+        public async Task<PowerSrcInfoDto> Handle(FindPowerCounterQuery request, CancellationToken cancellationToken)
+        {
+            var powerCounterEntity = await _context.PowerSrcInfo.Include(c => c.TargetCounter).FirstOrDefaultAsync(c => c.bill_identifier == request.TagIdentity);
+            PowerSrcInfoDto dto = _mapper.Map<PowerSrcInfoDto>(powerCounterEntity);
+            return dto;
+        }
+    }
+}
